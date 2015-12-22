e302246d1a00df736ebcf5cc3a89f1e01d178ac1
nicolas <zepouet@gmail.com>
2015-12-22 19:49:02 +0100

    Correction for docker-machine install : use official install from docker website

--- cu-infrastructure/shell/provision.sh
+++ cu-infrastructure/shell/provision.sh
@@ -10,45 +10,18 @@ readonly MACHINE_VERSION=v0.5.1
 
 #==========================================================#
 
-#
-# error handling
-#
-
-catch_error() {
-    echo { \"status\": $RETVAL, \"error_line\": $BASH_LINENO }
-}
-
-trap 'RETVAL=$?; echo "ERROR"; catch_error '  ERR
-trap 'RETVAL=$?; echo "received signal to stop";  catch_error ' SIGQUIT SIGTERM SIGINT
-
-#==========================================================#
-
 cp -f cloudunit/cu-infrastructure/files/.bashrc /home/vagrant/.bashrc
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo cp -f cloudunit/cu-infrastructure/files/sources.list /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y apt-transport-https
-sudo apt-get install -y curl unzip
+sudo apt-get install -y curl
 sudo apt-get install -y golang
 
 #==========================================================#
 
-#---------------------------------------#
-# fix base box
-#
-
-sudo cat <<-HOSTNAME > /etc/hostname
-  cloudunit.dev
-HOSTNAME
-
-cat <<-EOBASHRC  >> /home/vagrant/.bashrc
-  export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w \$\[\033[00m\] '
-  export LC_CTYPE=C.UTF-8
-EOBASHRC
-
 # clean up
-sudo apt-get clean
 sudo rm -f \
   /var/log/messages   \
   /var/log/lastlog    \
@@ -81,7 +54,7 @@ sudo systemctl start  docker
 sudo cp cloudunit/cu-infrastructure/files/grub /etc/default/grub
 sudo update-grub
 
-# install Docker Compose (was: Fig)
+# install Docker Compose
 # @see http://docs.docker.com/compose/install/
 curl -o docker-compose -L https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m`
 chmod a+x docker-compose
@@ -89,11 +62,9 @@ sudo mv docker-compose /usr/local/bin
 
 # install Docker Machine
 # @see https://docs.docker.com/machine/
-curl -o docker-machine.zip -L https://github.com/docker/machine/releases/download/$MACHINE_VERSION/docker-machine_linux-amd64.zip
-unzip docker-machine.zip
-rm docker-machine.zip
-chmod a+x docker-machine*
-sudo mv docker-machine* /usr/local/bin
+sudo curl -L https://github.com/docker/machine/releases/download/$MACHINE_VERSION/docker-machine_linux-amd64 > docker-machine
+chmod +x docker-machine
+sudo mv docker-machine /usr/local/bin/
 
 # install swarm
 sudo docker pull swarm
