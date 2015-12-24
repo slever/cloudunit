1e7067bab3792a4f9652ad416520468a9309fe44
Guillaume <g.martial@treeptik.fr>
2015-12-24 16:27:26 +0100

    REFACTOR-implements maven parent structure

--- cu-core/src/main/java/fr/treeptik/cloudunit/core/utils/JsonDateSerializer.java
+++ cu-core/src/main/java/fr/treeptik/cloudunit/core/utils/JsonDateSerializer.java
@@ -13,30 +13,28 @@
  * For any questions, contact us : contact@treeptik.fr
  */
 
-package fr.treeptik.cloudunit.utils;
+package fr.treeptik.cloudunit.core.utils;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@Component
 public class JsonDateSerializer
-    extends JsonSerializer<Date> {
+        extends JsonSerializer<Date> {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat(
-        "yyyy-MM-dd HH:mm");
+            "yyyy-MM-dd HH:mm");
 
     @Override
     public void serialize(Date date, JsonGenerator gen,
                           SerializerProvider provider)
-        throws IOException,
-        JsonProcessingException {
+            throws IOException,
+            JsonProcessingException {
 
         String formattedDate = dateFormat.format(date);
 
