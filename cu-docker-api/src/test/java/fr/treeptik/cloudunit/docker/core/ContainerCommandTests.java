e4ed2fbb48723900315b8cd0fc247c3e18f89f93
Guillaume <g.martial@treeptik.fr>
2016-01-11 12:06:51 +0100

    TEST-fix all tests for container

--- cu-docker-api/src/test/java/fr/treeptik/cloudunit/docker/core/ContainerCommandTests.java
+++ cu-docker-api/src/test/java/fr/treeptik/cloudunit/docker/core/ContainerCommandTests.java
@@ -7,10 +7,14 @@ import fr.treeptik.cloudunit.docker.model.Config;
 import fr.treeptik.cloudunit.docker.model.Container;
 import fr.treeptik.cloudunit.docker.model.HostConfig;
 import fr.treeptik.cloudunit.exception.DockerJSONException;
+import fr.treeptik.cloudunit.utils.ContainerUtils;
 import org.junit.*;
 import org.junit.runners.MethodSorters;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by guillaume on 21/10/15.
@@ -24,7 +28,7 @@ public class ContainerCommandTests {
     private static DockerClient dockerClient;
     private static final String CONTAINER_NAME = "myContainer";
     private final int RUNNING_CONTAINERS = 7;
-    private Container container;
+    private static Container container;
 
     @BeforeClass
     public static void setupClass() {
@@ -69,9 +73,10 @@ public class ContainerCommandTests {
 
     @After
     public void tearDown() throws Exception {
-        dockerClient.findContainer(container, DOCKER_HOST);
-        int status = dockerClient.removeContainer(container, DOCKER_HOST).getStatus();
-        Assert.assertEquals(status, 204);
+        Container container = ContainerBuilder.aContainer()
+                .withName(CONTAINER_NAME)
+                .build();
+        dockerClient.removeContainer(container, DOCKER_HOST);
     }
 
     @Test
@@ -81,20 +86,10 @@ public class ContainerCommandTests {
 
         dockerClient.findContainer(container, DOCKER_HOST);
 
-        HostConfig hostConfig = HostConfigBuilder.aHostConfig()
-                .withLinks(new ArrayList<>())
-                .withBinds(new ArrayList<>())
-                .withPortBindings(new HashMap<>())
-                .withPrivileged(Boolean.FALSE)
-                .withPublishAllPorts(Boolean.TRUE)
-                .withVolumesFrom(new ArrayList<>()).build();
-        Config config = ConfigBuilder.aConfig()
-                .withHostConfig(hostConfig)
-                .build();
-        container = ContainerBuilder.aContainer()
-                .withName(CONTAINER_NAME)
-                .withConfig(config).build();
+        container = ContainerUtils.newStartInstance(container.getName(), null, null, null);
+
         dockerClient.startContainer(container, DOCKER_HOST);
+
         Assert.assertTrue(dockerClient.findContainer(container, DOCKER_HOST).getState().getRunning());
 
         dockerClient.stopContainer(container, DOCKER_HOST);
@@ -102,127 +97,44 @@ public class ContainerCommandTests {
 
         dockerClient.startContainer(container, DOCKER_HOST);
         dockerClient.killContainer(container, DOCKER_HOST);
-        Assert.assertEquals(new Long(137), dockerClient.findContainer(container, DOCKER_HOST).getState().getExitCode());
+        dockerClient.startContainer(container, DOCKER_HOST);
+
     }
 
     @Test
-    public void test20_createContainerWithVolumeFrom() throws DockerJSONException {
-        HostConfig hostConfig = HostConfigBuilder.aHostConfig()
-                .withVolumesFrom(new ArrayList() {{
-                    add("tomcat-8");
-                }})
-                .build();
-        Config config = ConfigBuilder.aConfig()
-                .withAttachStdin(Boolean.FALSE)
-                .withAttachStdout(Boolean.TRUE)
-                .withAttachStderr(Boolean.TRUE)
-                .withCmd(Arrays.asList("/bin/bash", "/cloudunit/scripts/start-service.sh", "johndoe", "abc2015",
-                        "192.168.2.116", "172.17.0.221", "aaaa",
-                        "AezohghooNgaegh8ei2jabib2nuj9yoe", "main"))
-                .withImage("cloudunit/git:dev")
-                .withHostConfig(hostConfig)
-                .withExposedPorts(new HashMap<>())
-                .withMemory(0L)
-                .withMemorySwap(0L)
-                .build();
-        Container container = ContainerBuilder.aContainer()
-                .withName(CONTAINER_NAME + "" + new Random().nextInt())
-                .withConfig(config)
-                .build();
-        dockerClient.createContainer(container, DOCKER_HOST);
+    public void test20_createContainerWithVolumeFrom() throws DockerJSONException, InterruptedException {
+        container = ContainerUtils.newStartInstance(container.getName(), null, Arrays.asList("tomcat-8"), null);
         dockerClient.startContainer(container, DOCKER_HOST);
-
         container = dockerClient.findContainer(container, DOCKER_HOST);
         Assert.assertTrue(dockerClient.execCommand(container, Arrays.asList("bash", "-c", "ls /cloudunit/binaries"), DOCKER_HOST).getBody()
-                .contains("bin"));
-        dockerClient.findContainer(container, DOCKER_HOST);
+               .contains("bin"));
+        Assert.assertTrue(dockerClient.execCommand(container, Arrays.asList("date"), DOCKER_HOST).getBody()
+                .contains("2016"));
+        container = ContainerUtils.newStartInstance(container.getName(), null, null, null);
         dockerClient.killContainer(container, DOCKER_HOST);
-        dockerClient.removeContainer(container, DOCKER_HOST);
+
+
     }
 
     @Test
     public void test30_createContainerWithVolumes() throws DockerJSONException {
-        HostConfig hostConfig = HostConfigBuilder.aHostConfig()
-                .withBinds(new ArrayList() {{
-                    add("/etc/localtime:/etc/localtime:ro");
-                }})
-                .build();
-        Config config = ConfigBuilder.aConfig()
-                .withAttachStdin(Boolean.FALSE)
-                .withAttachStdout(Boolean.TRUE)
-                .withAttachStderr(Boolean.TRUE)
-                .withCmd(Arrays.asList("/bin/bash", "/cloudunit/scripts/start-service.sh", "johndoe", "abc2015",
-                        "192.168.2.116", "172.17.0.221", "aaaa",
-                        "AezohghooNgaegh8ei2jabib2nuj9yoe", "main"))
-                .withImage("cloudunit/git:dev")
-                .withHostConfig(hostConfig)
-                .withExposedPorts(new HashMap<>())
-                .withMemory(0L)
-                .withMemorySwap(0L)
-                .build();
-        Container container = ContainerBuilder.aContainer()
-                .withName(CONTAINER_NAME + "" + new Random().nextInt())
-                .withConfig(config)
-                .build();
-        dockerClient.createContainer(container, DOCKER_HOST);
+        container = ContainerUtils.newStartInstance(container.getName(),
+                null, null, Arrays.asList("/etc/localtime:/etc/localtime:ro"));
         dockerClient.startContainer(container, DOCKER_HOST);
-
-        container = dockerClient.findContainer(container, DOCKER_HOST);
         List mounts = dockerClient.findContainer(container, DOCKER_HOST).getMounts();
         Assert.assertTrue(mounts.toString().contains("localtime"));
-        dockerClient.findContainer(container, DOCKER_HOST);
-        dockerClient.killContainer(container, DOCKER_HOST);
-        dockerClient.removeContainer(container, DOCKER_HOST);
     }
 
     @Test
     public void test40_startContainerWithPorts() throws DockerJSONException {
-
-        /*
-        Ports definition 22/tcp -> 2000
-         */
-
-        String hostPort = "2000";
-        String hostIP = "172.17.42.1";
-        String portKey = "22/tcp";
-
-        Map<String, String> mapForHostPort = new HashMap() {{
-            put("HostPort", hostPort);
-        }};
-
-
-        List<Map<String, String>> params = Arrays.asList(mapForHostPort);
-
-
-        HostConfig hostConfig = HostConfigBuilder.aHostConfig()
-                .withBinds(new ArrayList())
-                .withPortBindings(new HashMap() {{
-                    put(portKey, params);
-                }})
-                .build();
-        Config config = ConfigBuilder.aConfig()
-                .withAttachStdin(Boolean.FALSE)
-                .withAttachStdout(Boolean.TRUE)
-                .withAttachStderr(Boolean.TRUE)
-                .withCmd(Arrays.asList("/bin/bash", "/cloudunit/scripts/start-service.sh", "johndoe", "abc2015",
-                        "192.168.2.116", "172.17.0.221", "aaaa",
-                        "AezohghooNgaegh8ei2jabib2nuj9yoe", "main"))
-                .withImage("cloudunit/git:dev")
-                .withHostConfig(hostConfig)
-                .withExposedPorts(new HashMap<>())
-                .withMemory(0L)
-                .withMemorySwap(0L)
-                .build();
-        Container container = ContainerBuilder.aContainer()
-                .withName(CONTAINER_NAME + "" + new Random().nextInt())
-                .withConfig(config)
-                .build();
-        dockerClient.createContainer(container, DOCKER_HOST);
+        container = ContainerUtils.newStartInstance(container.getName(),
+                new HashMap() {{
+                    put("22/tcp", "2000");
+                }}, null,
+                null);
         dockerClient.startContainer(container, DOCKER_HOST);
+        Assert.assertTrue((dockerClient.findContainer(container, DOCKER_HOST)
+                .getNetworkSettings().getPorts().toString().contains("2000")));
 
-        container = dockerClient.findContainer(container, DOCKER_HOST);
-        dockerClient.findContainer(container, DOCKER_HOST);
-        dockerClient.killContainer(container, DOCKER_HOST);
-        dockerClient.removeContainer(container, DOCKER_HOST);
     }
 }
