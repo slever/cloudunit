44df2efe86e8d46f9416f406a19d96c1a410f90e
slever <sebastien.lever@gmail.com>
2016-08-31 15:55:48 +0200

    #147 add proxy support to build-service.sh

--- cu-services/build-services.sh
+++ cu-services/build-services.sh
@@ -3,11 +3,23 @@
 
 CACHE=false
 
-if [ $CACHE = true ]; then
-	BUILD_CMD="docker build --rm -t"
-elif [ $CACHE = false ]; then
-	BUILD_CMD="docker build --rm --no-cache -t"
+BUILD_CMD="docker build --rm"
+
+if [ $CACHE = false ]; then
+	BUILD_CMD="$BUILD_CMD --no-cache"
 fi
+
+if [ $http_proxy ]; then
+	echo "Use http proxy: $http_proxy"
+	BUILD_CMD="$BUILD_CMD --build-arg http_proxy=$http_proxy"
+fi
+
+if [ $https_proxy ]; then
+        echo "Use https proxy: $https_proxy"
+        BUILD_CMD="$BUILD_CMD --build-arg https_proxy=$https_proxy"
+fi
+
+BUILD_CMD="$BUILD_CMD -t"
 
 GIT_TAG=latest
 
@@ -15,7 +27,7 @@ while read line
 do
 	image=`echo $line | cut -d" " -f 1`
 	repository=`echo $line | cut -d" " -f 2`
-	echo -e "\n Building image $image:$GIT_TAG."
+	echo -e "\n Building image $image:$GIT_TAG.($BUILD_CMD)"
 	$BUILD_CMD $image:$GIT_TAG $repository
 	docker images | grep $image | grep $GIT_TAG
 	return=$?
